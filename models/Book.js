d8970b54c458a7f684ab7a0d1cf281a3a653c4b7
Velrin Black <velrinblack@gmail.com>
2022-11-21 20:19:33 +0100

    Create /book/getFiltered route and add nodemon

---
+++ models/Book.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const bookSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  ISBN: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  yearPublished: {
+    type: String,
+    required: true,
+  },
+  state: {
+    type: String,
+    required: true,
+  },
+});
+
+const Book = mongoose.model('Book', bookSchema);
+
+module.exports = Book;
---
+++ routes/book.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const Book = require('../models/Book');
+const { responseMessages } = require('../strings.json');
+
+const router = express.Router();
+
+router.get('/getFiltered', async (req, res) => {
+  if (req.query.title) {
+    let books = await Book.find();
+    books = books.filter((book) => book.title.toLowerCase() === req.query.title.toLowerCase());
+    if (!books.length) return res.status(404).json({ message: responseMessages.notFound });
+    return res.status(200).json({ books });
+  } else if (req.query.ISBN) {
+    let books = await Book.find({ ISBN: req.query.ISBN });
+    if (!books.length) return res.status(404).json({ message: responseMessages.notFound });
+    return res.status(200).json({ books });
+  } else {
+    return res.status(400).json({ message: responseMessages.invalidParameters });
+  }
+});
+
+module.exports = router;
