ce4b47e8839178c1b7ab932cc4154a52624b9bcb
Velrin Black <velrinblack@gmail.com>
2023-01-14 15:24:43 +0100

    Fix the getFiltered route so it checks both title and ISBN if both of the parameters are given

--- routes/book.js
+++ routes/book.js
@@ -5,15 +5,36 @@ const { responseMessages } = require('../strings.json');
 const router = express.Router();
 
 router.get('/getFiltered', async (req, res) => {
-  if (req.query.title) {
-    let books = await Book.find();
-    books = books.filter((book) => book.title.toLowerCase() === req.query.title.toLowerCase());
-    if (!books.length) return res.status(404).json({ message: responseMessages.notFound });
-    return res.status(200).json({ books });
-  } else if (req.query.ISBN) {
-    let books = await Book.find({ ISBN: req.query.ISBN });
-    if (!books.length) return res.status(404).json({ message: responseMessages.notFound });
-    return res.status(200).json({ books });
+  const { title, isbn } = req.query;
+
+  if (title || isbn) {
+    if (title && isbn) {
+      let books = await Book.find();
+
+      let booksByTitle = books.filter((book) => book.title.toLowerCase() === title.toLowerCase());
+      let booksByISBN;
+
+      if (booksByTitle.length) {
+        return res.status(200).json({ books: booksByTitle });
+      } else {
+        booksByISBN = await Book.find({ ISBN: isbn });
+
+        if (booksByISBN.length) {
+          return res.status(200).json({ books: booksByISBN });
+        } else {
+          return res.status(404).json({ message: responseMessages.notFound });
+        }
+      }
+    } else if (title) {
+      let books = await Book.find();
+      books = books.filter((book) => book.title.toLowerCase() === title.toLowerCase());
+      if (!books.length) return res.status(404).json({ message: responseMessages.notFound });
+      return res.status(200).json({ books });
+    } else {
+      let books = await Book.find({ ISBN: isbn });
+      if (!books.length) return res.status(404).json({ message: responseMessages.notFound });
+      return res.status(200).json({ books });
+    }
   } else {
     return res.status(400).json({ message: responseMessages.invalidParameters });
   }
